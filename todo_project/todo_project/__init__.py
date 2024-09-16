706524ff40a6f525f77e3bf6edac082a7eb30f0b
jeanersilva <jeanerufmg@gmail.com>
2024-09-16 13:32:10 -0500

    Ajusta sqlite

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+print(os.getenv('SECRET_KEY'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
+
+@app.before_request
+def create_tables():
+    db.create_all()
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' 
